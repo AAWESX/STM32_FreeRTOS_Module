2defb3c31aa4b18ff4f4317f7acee2f15178164b
AAWESX <3265103350@qq.com>
2024-03-17 18:14:31 +0800

    first commit

---
+++ FreeRTOS_FoundationProject/MDK-ARM/FreeRTOS_FoundationProject/port.d
@@ -0,0 +1,12 @@
+freertos_foundationproject\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM3/port.c
+freertos_foundationproject\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+freertos_foundationproject\port.o: D:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stddef.h
+freertos_foundationproject\port.o: D:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdint.h
+freertos_foundationproject\port.o: ../Core/Inc/FreeRTOSConfig.h
+freertos_foundationproject\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+freertos_foundationproject\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+freertos_foundationproject\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+freertos_foundationproject\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM3/portmacro.h
+freertos_foundationproject\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+freertos_foundationproject\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+freertos_foundationproject\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
